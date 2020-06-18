651563fbb603c3e0ec9f47b8f903c15878ef633c
TeddyJubu <syeedmdjobayer@gmail.com>
2020-06-18 20:34:53 +0600

    2 page separated

---
+++ lib/input_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        height: 200.0,
+        width: 170.0,
+        decoration: BoxDecoration(
+          color: Color(0xff1D1F33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
